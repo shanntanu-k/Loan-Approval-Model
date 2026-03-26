dfa6abd8c881162ae68c8d2b0667c253b5d0a274
shanntanu-k <shanntanuk@gmail.com>
2026-03-26 05:37:16 +0530

    comments added

--- main.py
+++ main.py
@@ -122,3 +122,5 @@ def predict() -> tuple[Any, int]:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
+# 3.11 venv with pip install flask scikit-learn joblib pandas 
